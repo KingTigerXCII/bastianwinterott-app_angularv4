7de2606dcdbbb5087a3c29af2a54bd814d1eb305
hellobasti <bastianwinterott@gmail.com>
2017-06-05 19:39:19 +0200

    nothing special, just text was changed

--- src/app/bastianwinterott-app/components/content/welcome/welcome.component.ts
+++ src/app/bastianwinterott-app/components/content/welcome/welcome.component.ts
@@ -14,16 +14,14 @@ export class WelcomeComponent implements OnInit {
 
     let article: Article = {
       createdDate: new Date('February 4, 2016 10:13:00'),
-      headline: 'Welcome',
-      text: 
-      `
-        Hi and welcome to my website. My name is Bastian, I'm 24 and I work as a software developer in 
-        Nuremberg, Germany. Software developing isn't only my job, it also influences my private life. 
-        Therefore I'm very interested in new technologies and how they work. As programming is one of 
-        my passions, this website is meant to keep you updated about my private side projects.
-        Thank you for visiting my website. I hope you like it.
-      `
-    }
+      headline: 'The First',
+      text:
+        `
+          Hi and welcome to my website. If you want more information about me and my work, you will find 
+          the 'Aboutme' and 'projects' section in the menu. I love to share my code too, you will find some
+          code exambles on my github page. 
+        `
+    };
 
     this.articles.push(article);
 
